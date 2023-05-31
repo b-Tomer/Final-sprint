92a62fda305e60bb92ad54ecca8525cc0e46ecd4
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-05-31 23:06:52 +0300

    restore corrupted package.lock

--- frontend/src/pages/board-index.jsx
+++ frontend/src/pages/board-index.jsx
@@ -8,7 +8,8 @@ import {
 } from '../store/board.actions.js'
 
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
-import { groupService } from '../services/group.service.js'
+import { boardService } from '../services/board.service.local.js'
+import { useParams } from 'react-router-dom'
 
 export function BoardIndex() {
     // const boards = useSelector(storeState => storeState.boardModule.boards)
@@ -17,7 +18,7 @@ export function BoardIndex() {
     const { board } = useSelector((storeState) => storeState.boardModule)
 
     useEffect(() => {
-        loadGroups()
+        onLoadBoard()
     }, [])
 
     function onLoadBoard() {
@@ -26,23 +27,24 @@ export function BoardIndex() {
 
     async function onRemoveBoard(boardId) {
         try {
-            await removeGroup(groupId)
-            showSuccessMsg('Group removed')
+            await removeBoard(boardId)
+            showSuccessMsg('Board removed')
         } catch (err) {
-            showErrorMsg('Cannot remove group')
+            showErrorMsg('Cannot remove board')
         }
     }
 
-    async function onAddGroup() {
-        const group = groupService.getEmptyGroup()
-        group.vendor = prompt('Vendor?')
+    async function onAddBoard() {
+        const board = boardService.getEmptyBoard()
+        board.vendor = prompt('Vendor?')
         try {
-            const savedGroup = await addGroup(group)
-            showSuccessMsg(`Group added (id: ${savedGroup._id})`)
+            const savedBoard = await addBoard(board)
+            showSuccessMsg(`Board added (id: ${savedBoard._id})`)
         } catch (err) {
-            showErrorMsg('Cannot add group')
+            showErrorMsg('Cannot add board')
         }
     }
+    if (board) console.log(board)
 
     return (
         <section className="board-container">
