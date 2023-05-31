e30f7db7ad25e50cf43af90e6cfa7235920e6c05
T0mer007 <tomerbenaim1@gmail.com>
2023-05-31 23:14:16 +0300

    a

--- frontend/src/cmps/group-details.jsx
+++ frontend/src/cmps/group-details.jsx
@@ -1,22 +1,14 @@
+<<<<<<< HEAD
+import { TaskPreview } from "./task-preview";
 
-    return (
-        <div>
-            <div>
-                <h1>{group.title}</h1>
-                <button>...</button>
-            </div>
-            {group.tasks.map(task =>
-                <div className="task-preview" key={task.id}>
-                    <TaskPreview task={task} />
-                </div>
-            )}
-            <div>
-                <button>+ Add a card</button>
-                <button>%</button>
-            </div>
-        </div>
-    )
-}
+=======
+>>>>>>> 92a62fda305e60bb92ad54ecca8525cc0e46ecd4
+export function GroupDetails({ group }) {
+
+
+
+
+
     return (
         <div>
             <div>
