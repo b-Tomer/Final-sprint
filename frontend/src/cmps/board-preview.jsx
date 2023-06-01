e026e083da27c5aa9a4034288ed4d2480e38f43c
ohadMazza <ohadmazza@gmail.com>
2023-06-01 23:26:43 +0300

    add yellow stars to workspace

--- frontend/src/cmps/board-preview.jsx
+++ frontend/src/cmps/board-preview.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import Background1 from '../../src/assets/img/backgrounds/asset0.jpeg'
 import Background2 from '../../src/assets/img/backgrounds/asset1.jpeg'
 import Background3 from '../../src/assets/img/backgrounds/asset2.jpeg'
@@ -7,11 +10,12 @@ import Background5 from '../../src/assets/img/backgrounds/asset5.jpeg'
 import Background6 from '../../src/assets/img/backgrounds/asset6.jpeg'
 import Background7 from '../../src/assets/img/backgrounds/asset7.jpeg'
 
-import { useNavigate } from 'react-router-dom'
 
 import { ReactComponent as Starred } from '../assets/img/icons/starred.svg'
+import { ReactComponent as StarredYellow } from '../assets/img/icons/starred-yellow.svg'
 
-export function BoardPreview({ board }) {
+export function BoardPreview({ board, toggleStarredStatus }) {
+    // const [isStarred, setIsStarred] = useState(false);
     ////////////////////////////  CHANGE TO CLOUDINARY /////////////////////////////
     const images = [Background1, Background2, Background3, Background4, Background5, Background6, Background7]
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -20,18 +24,28 @@ export function BoardPreview({ board }) {
 
     const navigate = useNavigate()
 
+    const buttonClassName = board.isStarred ? 'starred-btn starred' : 'starred-btn';
+
+    // useEffect(() => {
+    // }, [board.isStarred])
+
     function onClickBoard() {
         navigate(`/board/${board._id}`)
         console.log(board._id, 'board._id')
     }
 
+    const onClickStarred = (event) => {
+        event.stopPropagation()
+        toggleStarredStatus(board)
+    }
 
     return (
-
         <div className="board-preview" onClick={onClickBoard}>
             <h1>{board.title}</h1>
             <img src={randomImage} alt="Logo" />
-            <button> <Starred className='starred-btn' /></button>
+            <button className={buttonClassName} onClick={onClickStarred}>
+                {board.isStarred ? <StarredYellow /> : <Starred />}
+            </button>
         </div>
     )
 }
