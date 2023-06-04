5e6cdb5b133cbf6c7149153bb6b5c82f907fb4ac
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-06-05 00:12:03 +0300

    minor fix

--- frontend/src/cmps/task/task-details.jsx
+++ frontend/src/cmps/task/task-details.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 import { TaskMenu } from './task-menu.jsx'
 import { TaskMainDetails } from './task-main-details.jsx'
 import { TaskHeader } from './task-header.jsx'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useLayoutEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { ReactComponent as X } from '../../assets/img/icons/x.svg'
@@ -25,11 +25,40 @@ export function TaskDetails({
     )
     const [layout, setLayout] = useState(undefined)
     const [scroll, setScroll] = useState(true)
+    const elementRef = useRef(null)
 
     useEffect(() => {
         setTask(currTask)
         setGroup(board.groups[groupIdx])
     }, [board])
+
+    const measureHeight = () => {
+        if (elementRef.current) {
+            const height = elementRef.current.clientHeight
+            console.log('Element height:', height)
+        }
+    }
+
+    useLayoutEffect(() => {
+        measureHeight()
+
+        const handleResize = () => {
+            measureHeight()
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    // useLayoutEffect(() => {
+    //     if (elementRef.current) {
+    //         // const height = elementRef.current.clientHeight
+    //         // console.log('Element height:', height)
+    //         console.log(elementRef.current.getBoundingClientRect())
+    //     }
+    // }, [])
 
     function closeModal(ev, ref) {
         if (ref.current !== ev.target) {
@@ -54,18 +83,23 @@ export function TaskDetails({
                 closeModal(ev, taskOverlayRef)
             }}
         >
-            <section className="task-details-container">
-                <TaskCover simpleCloseModal={simpleCloseModal} task={task} />
-                <div className="task-details-secondry-container">
-                    <TaskHeader task={task} group={group} />
-                    <TaskMainDetails
-                        boardId={boardId}
+            <div className="task-details-parent">
+                <section className="task-details-container" ref={elementRef}>
+                    <TaskCover
+                        simpleCloseModal={simpleCloseModal}
                         task={task}
-                        groupId={groupId}
                     />
-                    <TaskMenu />
-                </div>
-            </section>
+                    <div className="task-details-secondry-container">
+                        <TaskHeader task={task} group={group} />
+                        <TaskMainDetails
+                            boardId={boardId}
+                            task={task}
+                            groupId={groupId}
+                        />
+                        <TaskMenu />
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
