fb88150a60d2168fc0bdb796c5f794e50863b5bf
ohadMazza <ohadmazza@gmail.com>
2023-06-05 10:55:28 +0300

    dyn cmps

--- frontend/src/cmps/dynamic-cmp/dyn-cmp-members.jsx
+++ frontend/src/cmps/dynamic-cmp/dyn-cmp-members.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from 'react-redux'
+
 
 export function DynCmpMembers() {
+    const { board } = useSelector((storeState) => storeState.boardModule)
 
 
 
     return (
         <h3>Board members</h3>
+
+
+
+
     )
 }
--- frontend/src/cmps/task/task-main-details.jsx
+++ frontend/src/cmps/task/task-main-details.jsx
@@ -2,7 +2,7 @@ import { TaskData } from './task-data.jsx'
 import { TaskMainContent } from './task-main-content.jsx'
 import { TaskMenu } from './task-menu.jsx'
 
-export function TaskMainDetails({ task, groupId ,boardId }) {
+export function TaskMainDetails({ task, groupId, boardId }) {
     return (
         <section className="task-main-detiails">
             <TaskData task={task} />
