f0c7f2331b30e3566e0784b0aebed6703cc0a120
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-06-06 12:53:09 +0300

    added support for todo edit

---
+++ frontend/src/cmps/task/checklists/todo-content.jsx
@@ -0,0 +1,42 @@
+export function TodoContent({
+    todo,
+    handleMouseEnter,
+    handleMouseLeave,
+    handleCheckboxChange,
+    checklist,
+    isHovered,
+    onDeleteTodo,
+    Trash,
+    selectTodoToEdit,
+}) {
+    return (
+        <div
+            onMouseEnter={() => handleMouseEnter(todo.id)}
+            onMouseLeave={handleMouseLeave}
+            className={`todo ${todo.isDone ? 'completed' : ''}`}
+            key={todo.id}
+        >
+            <div className="todo-content">
+                <input
+                    type="checkbox"
+                    id="todo"
+                    name="todo"
+                    checked={todo.isDone}
+                    onChange={() => handleCheckboxChange(checklist.id, todo.id)}
+                />
+                <h3 onClick={(event) => selectTodoToEdit(event, todo)}>
+                    {todo.title}
+                </h3>
+            </div>
+
+            {isHovered === todo.id && (
+                <button
+                    onClick={(event) => onDeleteTodo(event, checklist, todo)}
+                    className="delete-todo-btn"
+                >
+                    <Trash className="delete-todo-icon" />
+                </button>
+            )}
+        </div>
+    )
+}
