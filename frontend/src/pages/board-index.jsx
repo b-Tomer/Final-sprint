f8dc7a6ec86c10307e13f6b6d97e03f12c2f13b6
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-06-08 13:59:24 +0300

    UI improvmnts

--- frontend/src/pages/board-index.jsx
+++ frontend/src/pages/board-index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import {
     loadBoards,
     loadBoard,
--- frontend/src/services/task.service.local.js
+++ frontend/src/services/task.service.local.js
@@ -1,6 +1,5 @@
-import { boardService } from "./board.service.local"
-import { utilService } from "./util.service"
-
+import { boardService } from './board.service.local'
+import { utilService } from './util.service'
 
 export const taskService = {
     removeTask,
@@ -22,18 +21,19 @@ function getDefaultTask() {
     return task
 }
 
-
 async function saveTask(task, boardId, groupId) {
     const board = await boardService.getById(boardId)
     if (task.id) {
-        const groupIdx = board.groups.findIndex(group => groupId === group.id)
-        const taskIdx = board.groups[groupIdx].tasks.findIndex(currTask => currTask.id === task.id)
+        const groupIdx = board.groups.findIndex((group) => groupId === group.id)
+        const taskIdx = board.groups[groupIdx].tasks.findIndex(
+            (currTask) => currTask.id === task.id
+        )
         board.groups[groupIdx].tasks[taskIdx] = task
         board = await boardService.save(board)
         return board
     } else {
         task.id = utilService.makeId()
-        const idx = board.groups.findIndex(group => groupId === group.id)
+        const idx = board.groups.findIndex((group) => groupId === group.id)
         board.groups[idx].tasks.push(task)
         await boardService.save(board)
         return board
@@ -41,20 +41,22 @@ async function saveTask(task, boardId, groupId) {
 }
 
 async function updateTask(boardId, groupId, task) {
-    console.log('task from service: ', task )
     const board = await boardService.getById(boardId)
     const groupIdx = board.groups.findIndex((group) => group.id === groupId)
-    const taskIdx = board.groups[groupIdx].tasks.findIndex(currTask => currTask.id === task.id)
+    const taskIdx = board.groups[groupIdx].tasks.findIndex(
+        (currTask) => currTask.id === task.id
+    )
     board.groups[groupIdx].tasks.splice(taskIdx, 1, task)
     await boardService.save(board)
-    console.log('board from service: ', board )
     return board
-  }
+}
 
 async function removeTask(boardId, groupId, taskId) {
     const board = await boardService.getById(boardId)
-    const groupIdx = board.groups.findIndex(group => groupId === group.id)
-    const taskIdx = board.groups[groupIdx].tasks.findIndex(task => taskId === task.id)
+    const groupIdx = board.groups.findIndex((group) => groupId === group.id)
+    const taskIdx = board.groups[groupIdx].tasks.findIndex(
+        (task) => taskId === task.id
+    )
     board.groups[groupIdx].tasks.splice(taskIdx, 1)
     await boardService.save(board)
     return board
