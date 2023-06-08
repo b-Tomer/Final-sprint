57ae012df67e5752f7427a8ced53f898300c1dc2
T0mer007 <tomerbenaim1@gmail.com>
2023-06-08 12:54:02 +0300

    added auto scrolling

--- frontend/src/cmps/task/members-preview.jsx
+++ frontend/src/cmps/task/members-preview.jsx
@@ -1,25 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-
 import { ReactComponent as Plus } from '../../assets/img/icons/plus.svg'
 import { store } from '../../store/store'
-import { CLOSE_DYN_ALL_MODALS, OPEN_DYN_MEMBER_MODAL, OPEN_DYN_MODAL, SET_MODAL_TITLE } from '../../store/system.reducer'
 import { DynamicCmp } from '../dynamic-cmp/dynamic-cmp'
-
-
-
+import { CLOSE_DYN_ALL_MODALS, OPEN_DYN_MEMBER_MODAL, OPEN_DYN_MODAL, SET_MODAL_TITLE } from '../../store/system.reducer'
 
 export function MembersPreview({ task }) {
   const { board } = useSelector((storeState) => storeState.boardModule)
   const { isOpenMemberModal } = useSelector((storeState) => storeState.systemModule)
 
-
-  function onOpenMembersModal(){
-    store.dispatch({ type: SET_MODAL_TITLE ,title: 'Members' })
+  function onOpenMembersModal() {
+    store.dispatch({ type: SET_MODAL_TITLE, title: 'Members' })
     store.dispatch({ type: CLOSE_DYN_ALL_MODALS })
     store.dispatch({ type: OPEN_DYN_MODAL })
     store.dispatch({ type: OPEN_DYN_MEMBER_MODAL })
-
   }
 
   function getMemberImg(memberId) {
@@ -41,13 +35,13 @@ export function MembersPreview({ task }) {
       <div className="members">
         {task.members.map(member => (
           < div key={member._id} >
-            <img src={getMemberImg(member)} alt="Image" className='member-img' />
+            <img key={member._id} src={getMemberImg(member)} alt="Image" className='member-img' />
           </div>
         )
         )}
         <div onClick={onOpenMembersModal} className='add-member-icon'><Plus className='plus-icon' /> </div>
       </div>
-      {isOpenMemberModal && <DynamicCmp task={task} title='Edit attachment'/>}
+      {isOpenMemberModal && <DynamicCmp task={task} title='Edit attachment' />}
     </div >
   )
 }
