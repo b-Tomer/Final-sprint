603ceea9c21a2f1be77bfdeb69a010883bfe3bfc
T0mer007 <tomerbenaim1@gmail.com>
2023-06-13 16:50:49 +0300

    spotify style changed and task details

--- frontend/src/cmps/spotify.jsx
+++ frontend/src/cmps/spotify.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react"
+import { ReactComponent as Play } from '../assets/img/icons/play.svg'
+
 
 export function Spotify() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isPlayerPlaying, setIsPlayerPlaying] = useState(false)
 
     function onToggleSpotify() {
         setIsMenuOpen(!isMenuOpen)
+        setIsPlayerPlaying(true)
     }
 
     return (
-        <aside className={`side-spotify ${isMenuOpen ? 'open-spotify' : ''}`}>
-            <button onClick={onToggleSpotify} className='open-spotify-btn'><i className="fa-brands fa-spotify" style={{ color: "#3db355" }}></i></button>
+        <div onClick={onToggleSpotify} className={`side-spotify ${isMenuOpen ? 'open-spotify' : ''}`}>
+            <button
+                onClick={onToggleSpotify}
+                className={`open-spotify-btn ${isPlayerPlaying ? 'heartbeat-animation' : ''}`}>
+                <Play className="play-icon" /></button>
             <iframe
+                onClick={onToggleSpotify}
                 className='spotify-player'
                 title="Spotify Embed: Recommendation Playlist "
                 src={`https://open.spotify.com/embed/playlist/6kOVbGuByjJPcyiEs5MGst?utm_source=generator&theme=0`}
                 width="100%"
                 height="100%"
-                style={{ height: '100%' }}
                 frameBorder="0"
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
-            />
-        </aside>
+                />
+        </div>
     )
 }
